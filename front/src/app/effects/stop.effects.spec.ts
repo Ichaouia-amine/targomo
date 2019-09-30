6b14b9004ae7dc02d9c4a9cb6716fa6c3deebfc6
Ichaouia-amine <ichaouia.amine@gmx.com>
2019-09-30 20:15:35 +0200

    clean unit test

--- front/src/app/effects/stop.effects.spec.ts
+++ front/src/app/effects/stop.effects.spec.ts
@@ -3,13 +3,16 @@ import { provideMockActions } from '@ngrx/effects/testing';
 import { Observable } from 'rxjs';
 
 import { StopEffects } from './stop.effects';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('StopEffects', () => {
+  // tslint:disable-next-line: prefer-const
   let actions$: Observable<any>;
   let effects: StopEffects;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       providers: [
         StopEffects,
         provideMockActions(() => actions$)
--- front/src/app/stop/stop-item/stop-item.component.spec.ts
+++ front/src/app/stop/stop-item/stop-item.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StopItemComponent } from './stop-item.component';
+import { ChartsModule } from 'ng2-charts';
 
 describe('StopItemComponent', () => {
   let component: StopItemComponent;
@@ -8,6 +9,7 @@ describe('StopItemComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ChartsModule],
       declarations: [ StopItemComponent ]
     })
     .compileComponents();
@@ -16,6 +18,22 @@ describe('StopItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(StopItemComponent);
     component = fixture.componentInstance;
+    component.stop = {
+      stop_id: 'cca80ef6-8948-444d-af74-580da16f0177',
+      name: 'U Osloer Str. (Berlin)',
+      people_off: 150,
+      people_on: 0,
+      prev_stop_id: 'null',
+      next_stop_id: '94ff93c4-bcd5-44b2-9630-b92fb1dcdfc3',
+      reach_pop_30_walk: 565,
+      reach_pop_30_bike: 6590,
+      lat: 52.557107,
+      lng: 13.373279,
+      geom: {
+        type: 'Point',
+        coordinates: []
+      }
+    };
     fixture.detectChanges();
   });
 
