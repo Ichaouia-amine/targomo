c93b3766e270eefd4520c3bbcd361219ed386336
Ichaouia-amine <ichaouia.amine@gmx.com>
2019-09-30 17:38:04 +0200

    clean unit test

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -24,13 +24,6 @@ fdescribe('AppComponent', () => {
   it(`should have as title 'front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.size).toBeGreaterThan(10);
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to front!');
+    expect(app.size).toBeGreaterThan(400);
   });
 });
--- front/src/app/stop/stop.component.spec.ts
+++ front/src/app/stop/stop.component.spec.ts
@@ -1,16 +1,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StopComponent } from './stop.component';
-
+import { MzCollapsibleModule, MzIconModule, MzIconMdiModule, MzRadioButtonModule, MzInputModule } from 'ngx-materialize';
+import { ChartsModule } from 'ng2-charts';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { StopItemComponent } from './stop-item/stop-item.component';
+import { StopService } from '../services/stop.service';
+import { Store } from '@ngrx/store';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { Stop } from '../model/stop';
 describe('StopComponent', () => {
   let component: StopComponent;
   let fixture: ComponentFixture<StopComponent>;
-
+  // let store: MockStore<{ loggedIn: boolean }>;
+  const initialState = { loggedIn: false };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StopComponent ]
+      imports: [BrowserModule,
+        FormsModule, MzCollapsibleModule,
+        ChartsModule,
+        MzIconModule,
+        MzIconMdiModule,
+        MzRadioButtonModule,
+        MzInputModule],
+      declarations: [ StopComponent, StopItemComponent ],
+      providers: [
+        {provide: StopService},
+        provideMockStore({ initialState })
+      ]
     })
     .compileComponents();
+    // store = TestBed.get<Station>(Store);
   }));
 
   beforeEach(() => {
